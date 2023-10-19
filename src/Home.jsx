d528b091454933f9ffbe617c81e9a1aa1467f5c1
aryan-edstem <aryan.c@edstem.com>
2023-10-19 11:06:44 +0530

    Initial Commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div>
+        <h1>Let's Check your Knowledge</h1>
+        <button>Let's Start</button>
+        </div>
+    )
+}
+
+export default Home
